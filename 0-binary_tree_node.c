c4563128bbae27186cc6071c383cdf25017b9e82
root <queisecarvalho@hotmail.com>
2023-05-17 15:22:20 -0700

    task 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *new_node;
 
 	new_node = malloc(sizeof(binary_tree_t));
-	
+
 	if (new_node == 0)
 		return (NULL);
 
