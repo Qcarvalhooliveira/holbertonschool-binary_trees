fd4085ded62b1da0d895b296e9f2106e2e62c54e
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-05-19 15:06:07 +0200

    task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -24,8 +24,10 @@ int binary_tree_balance(const binary_tree_t *tree)
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
+
 	if (tree == NULL)
 		return (0);
+
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
