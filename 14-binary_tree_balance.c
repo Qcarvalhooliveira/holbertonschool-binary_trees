ade2c309617810dbeca86c0ada888acf26360b6b
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-05-19 14:54:58 +0200

    task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -26,6 +26,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	return ((binary_tree_height(tree->left) + 1 + binary_tree_height(tree->right)));
+	return ((binary_tree_height(tree->left) + binary_tree_height(tree->right)));
 }
 
