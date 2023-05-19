69c7993ab7fe0f6c798fc0db9bf51fb335a19625
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-05-19 13:56:26 +0200

    task 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -13,9 +13,12 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-	
+	else if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
 	else
 	{	
-		return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+		return ((binary_tree_nodes(tree->left) + 1 + binary_tree_nodes(tree->right)));
 	}
 }
