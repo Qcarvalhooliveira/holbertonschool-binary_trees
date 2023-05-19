e5c416707bc7e578205cb86ba32b5c37f9121308
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-05-19 15:20:05 +0200

    task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,7 +11,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t left_height, right_height;
 
 	if (tree == NULL)
+	{
 		return (0);
+	}
 
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
